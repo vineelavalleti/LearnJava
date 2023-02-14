a26e96a4049a03a67ac9255abe17531d991ec089
vineelavalleti <vineela.valleti@gmail.com>
2023-02-14 14:45:41 -0500

    understand work

--- src/com/vineela/learn/Condition.java
+++ src/com/vineela/learn/Condition.java
@@ -3,23 +3,45 @@ package com.vineela.learn;
 public class Condition {
 
 	public static void main(String[] args) {
+		System.out.println(x(6));
+		System.out.println(x(2));
 		
-		int x=5;
-		int y=6;
-		int z=7;
+		int result = add(10,4,20);
 		
-
-
-		if(x>y)
-			System.out.println(x);
-
-		else if(y<z)
-			
-		System.out.println(y);
+		System.out.println(result);
 		
-		else
-		 
-			System.out.println(z);
+		
 	}
+	
+
+	public static int add(int a,int b, int c) {
+		
+		return a+b+c; 
+	}
+	
+	public static String x(int n) {
+		
+		String day= "";
+
+		if (n == 1)
+			day="monday";
+		else if (n == 2)
+			day="tuesday";
+		else if (n == 3)
+			day="wednesday";
+		else if (n == 4)
+			day="thursday";
+		else if (n == 5)
+			day="friday";
+		else if (n == 6)
+			day="saturday";
+		else
+			day="sunday";
+		
+		return day;
+		
+	}
+	
+	
 
 }
