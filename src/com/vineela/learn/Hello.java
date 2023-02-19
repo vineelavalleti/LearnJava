5584afe864145195f85fc3a82b134937e1cddfb2
vineelavalleti <vineela.valleti@gmail.com>
2023-02-19 12:31:46 -0500

    changed

--- src/com/vineela/learn/Hello.java
+++ src/com/vineela/learn/Hello.java
@@ -4,10 +4,26 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-System.out.println(12345);
-System.out.println(0>5);
-
-
+		System.out.println(12345);
+		System.out.println(0 > 5);
+		
+		
+		Calculator  calcul = new Calculator();
+		
+		int addResult = calcul.add(10, 20);
+		System.out.println("addResult == "+addResult);
+		
+		int subResult = calcul.sub(30, 17);
+		
+		int mulResult = calcul.mul(50, 4);
+		
+		String s = "Vineela";
+		System.out.println("Lenght of S is : :"+s.length());
+		
+		
+	
+		System.out.println("subResult == "+subResult);
+		System.out.println("mulResult == "+mulResult);
 
 	}
 
