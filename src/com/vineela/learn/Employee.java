5be508dbfe80378347503ba517a6dae152aedd61
vineelavalleti <vineela.valleti@gmail.com>
2023-02-27 17:43:29 -0500

    new

--- src/com/vineela/learn/Employee.java
+++ src/com/vineela/learn/Employee.java
@@ -1,17 +1,51 @@
 package com.vineela.learn;
 
 public class Employee {
-	int id;
-	String name;
 	
-	public Employee getEmployeeData() {
-		// Call DB and get Employee data 
-		Employee e = new Employee();
-		e.id = 100;
-		e.name="Srinu";
-		
-		return e;
+	private int id;
+	private String name;
+	private int sal;
+	private int bonus;
+	
+	
+	
+	public Employee() {
 		
 	}
 
+	public Employee(int id, String name, int sal, int bonus) {
+		
+		this.id = id;
+		this.name = name;
+		this.sal = sal;
+		this.bonus = bonus;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSal() {
+		return sal;
+	}
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+	public int getBonus() {
+		return bonus;
+	}
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+	
+	
+	
 }
