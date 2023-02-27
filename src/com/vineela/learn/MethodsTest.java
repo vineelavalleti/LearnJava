52bab9724c99645f298ea57339a6f7b8aa42209a
vineelavalleti <vineela.valleti@gmail.com>
2023-02-27 17:44:23 -0500

    new

--- src/com/vineela/learn/MethodsTest.java
+++ src/com/vineela/learn/MethodsTest.java
@@ -3,19 +3,34 @@ package com.vineela.learn;
 public class MethodsTest {
 
 	public static void main(String[] args) {
+
+		goodMorning();
 		
-		
-		Employee e = new Employee();
-		Employee edata = e.getEmployeeData();
-		System.out.println(edata.id);
-		
-		
-		
-		String s8 = "Deekshita Valleti";
-		System.out.println("Lenght of S8 is : :"+s8.length());
-		
+		MethodsTest m=new MethodsTest();
+			int result	=m.add();
+				System.out.println(result);
+
+	System.out.println(result=MethodsTest.sub());
+
+				
+	}
+	public static void goodMorning(){
+			
+	System.out.println("Good Morning Vineela");
 	}
 	
+ public int add(){
+	 int a=10;
+	 int b=10;
+	int result=a+b;
+	return result;
+	 }
+ public static int sub(){
+	 int a=10;
+	 int b=5;
+	int result=a-b;
+	return result;
+	 }
 	
-
 }
+
